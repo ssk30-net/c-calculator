7176f192dfca0bbd2b51c5c2e2427b9ad558e8bc
Manav Patnaik <manavpatnaik@hotmail.com>
2021-05-15 15:26:12 +0530

    Added Python calc and changed README

--- calculator.c
+++ calculator.c
@@ -1,41 +1,86 @@
-// Git/GitHub workshop: Hands on session
-// Program: Calculator
-// Author: Manav Patnaik
-
-// Import required header files
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-// Calculator function to be written here
-// Functionalities required:
-// 1. Add
-// 2. Subtract
-// 3. Multiply
-// 4. Divide
-// 5. Percentage
-// 6. Exponentiation
-// 7. Sine (sin)
-// 8. Cosine (cos)
-// 9. Tangent (tan)
-// 10. Square root
-// 11. Ceil
-// 12. Floor
-// 13. Absolute value
-// 14. Natural exponent (e^x)
-
-// Sample code for 1. Add
-double add(double a, double b) {
-    return a + b;
+// 1. Addition
+void calcAddition() {
+    printf("\nEnter two numbers: \n");
+    int a, b;
+    scanf("%d%d", &a, &b);
+    int result = a + b;
+    printf("The sum is %d\n", result);
 }
 
-// Insert your code below
+// Fill the rest of the functionality here!
+// 2. Subtraction
+// 3. Multiplication
+// 4. Division
+// 5. Exponentiation
+
+// 6. Sine
+void calcSine() {
+    printf("\nEnter the angle in degrees: ");
+    int angle;
+    scanf("%d", &angle);
+
+    // Convert degrees to radians
+    double radians = angle / 180.0 * 3.14;
+    double answer = sin(radians);
+
+    printf("The sine value is %f", answer);
+}
+
+// Fill the rest of the functionality here!
+// 7. Cosine
+// 8. Tangent
+// 9. Floor
+// 10. Ceiling
+// 11. Round
+// 12. Absolute value
 
 int main() {
-    // Initialize variables
+    int choice;
+    printf(
+        "Welcome to the Cool Calculator program!\n\n"
+        "1. Addition\n"
+        "2. Subtraction\n"
+        "3. Multiplication\n"
+        "4. Division\n"
+        "5. Exponentiation\n"
+        "6. Sine\n"
+        "7. Cosine\n"
+        "8. Tangent\n"
+        "9. Floor\n"
+        "10. Ceiling\n"
+        "11. Round\n"
+        "12. Absolute value\n\n"
+        "Enter your choice: ");
 
-    // Prompt user for input
+    scanf("%d", &choice);
 
-    // Perform appropriate function
+    switch (choice) {
+        // 1. Addition
+        case 1:
+            calcAddition();
+            break;
 
-    // Terminate program after finishing tasks
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+
+        // 6. Sine
+        case 6:
+            calcSine();
+            break;
+
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        default:
+            printf("Invalid choice!");
+    }
+    return 0;
 }
